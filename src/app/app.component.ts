a906aa248a4a13a0e0eb58714a90a9d6ffdee9a3
Navjot Dhanawat <n.dhanawat@splendornet.com>
2018-02-16 11:49:50 +0530

    Mosca MQTT web example initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+// declare var require: any;
 import { Component } from '@angular/core';
+import * as mqtt from 'mqtt';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  client: any;
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.client = mqtt.connect('ws://localhost:3000', {
+      clientId: 'chrome'
+    });
+
+    this.client.subscribe('presence');
+
+    console.log('Client publishing.. ');
+
+  }
+
+  publish() {
+    this.client.publish('presence', 'Client 1 is alive.. Test Ping! ' + Date());
+  }
 }
